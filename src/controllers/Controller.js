45d3ba95db90d8108c7f7dda2c4495e5733f3145
Beomtae <95863810+Beomtae@users.noreply.github.com>
2024-12-12 13:58:46 +0900

    ✨ feat: 사용자 입력 받기

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -4,7 +4,9 @@ import OutputView from '../views/OutputView.js';
 
 class Controller {
   static async run() {
-    console.log('hi');
+    const monthAndDay = await InputView.readMonthAndDay();
+    const normalDayWorkingList = await InputView.readNormalDayWorkingList();
+    const holidayWorkingList = await InputView.readHolidayWorkingList();
   }
 }
 
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -2,8 +2,16 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class InputView {
-  static readSomething() {
-    return Console.readLineAsync();
+  static readMonthAndDay() {
+    return Console.readLineAsync(`비상 근무를 배정할 월과 시작 요일을 입력하세요> `);
+  }
+
+  static readNormalDayWorkingList() {
+    return Console.readLineAsync(`평일 비상 근무 순번대로 사원 닉네임을 입력하세요> `);
+  }
+
+  static readHolidayWorkingList() {
+    return Console.readLineAsync(`휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> `);
   }
 }
 export default InputView;
