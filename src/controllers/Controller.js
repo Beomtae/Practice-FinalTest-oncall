2be0b71c40468262ba161f264ea7b97d43b58ae6
Beomtae <95863810+Beomtae@users.noreply.github.com>
2024-12-12 13:49:42 +0900

    📝 docs: README.md 작성

---
+++ src/controllers/Controller.js
@@ -0,0 +1,11 @@
+// Controller.js
+import InputView from '../views/InputView.js';
+import OutputView from '../views/OutputView.js';
+
+class Controller {
+  static async run() {
+    console.log('hi');
+  }
+}
+
+export default Controller;
